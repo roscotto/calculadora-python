c07e48c22d991caa14320f031208a4fd130d7f79
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2022-12-11 20:07:09 -0300

    subida de archivos

---
+++ calculadora.py
@@ -0,0 +1,25 @@
+#Crear una calculadora con las operaciones matemáticas básicas utilizando Python
+
+valor1 = 7
+valor2 = 4
+
+resultadoSuma = valor1 + valor2
+
+resultadoResta = valor1 - valor2
+
+resultadoMultiplicacion = valor1 * valor2
+
+resultadoDivision = valor1 / valor2
+
+#Método simple
+print(resultadoSuma)
+print(resultadoResta)
+print(resultadoMultiplicacion)
+print(resultadoDivision)
+
+#Método por formato
+print(f'La suma es: {resultadoSuma}, la resta es: {resultadoResta}, la multiplicacion es: {resultadoMultiplicacion}, la Division es: {resultadoDivision}')
+
+
+#Método Tradicional
+print("La suma es:" ,resultadoSuma, "la resta es: " ,resultadoResta," la multiplicacion es: ",resultadoMultiplicacion, "la Division es: ",resultadoDivision)
